8243cca1fdae166daf4c3769663a12fcac7761ac
ValenSilveira <silveirattini@gmail.com>
2025-04-03 22:49:29 +0000

    semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const Home = () => {
+	const [color, setColor] = useState("red");
+	const [isVioletVisible, setIsVioletVisible] = useState(false);
+
+
+	const toggleColor = () => {
+		// para cambiar los colores con el boton
+		if (color === "red") {
+		  setColor("yellow");
+		} else if (color === "yellow") {
+		  setColor("green");
+		} else  {
+		  setColor("red");
+		}
+		setIsVioletVisible(false);
+	  };
+
+	  const activateViolet = () => {
+		// Mostrar solo el color violeta
+		setColor("violet");
+		setIsVioletVisible(true);
+	};
+
+
+	return (
+		<div className="traffic-light-container">
+			<div className="poste"></div>
+			<div className="traffic-light">
+				<div 
+					className={`light red ${color === "red" ? "selected" : ""}`}
+					onClick={() => { setColor("red"); setIsVioletVisible(false); }}>
+				</div>
+				<div 
+					className={`light yellow ${color === "yellow" ? "selected" : ""}`}
+					onClick={() => { setColor("yellow"); setIsVioletVisible(false); }}>
+				</div>
+				<div 
+					className={`light green ${color === "green" ? "selected" : ""}`}
+					onClick={() => { setColor("green"); setIsVioletVisible(false); }}>
+				</div>
+				{isVioletVisible && (
+					<div className={`light violet ${color === "violet" ? "selected" : ""}`}>
+					</div>
+				)}
+			</div>
+			<button onClick={toggleColor}>Alternar Color</button>
+			<button onClick ={activateViolet}>Color Violeta</button>
 		</div>
-	);
+		
+	)
+	
 };
 
 export default Home;
